bd4cda2d76f5228829e40eb9d0b7ebf0121dd8c1
balinthuszar <44851578+StackedWeeb@users.noreply.github.com>
2020-05-09 20:31:42 +0200

    shop simulation and a few comments added

--- otpmobil/src/otpmobil/WebshopApplication.java
+++ otpmobil/src/otpmobil/WebshopApplication.java
@@ -6,11 +6,11 @@ public class WebshopApplication {
 	public static void main(String[] args) {
 		
 		Webshop webshop = new Webshop();
-		Item item1 = new Book(2849, "Emelkedés", "EURÓPA KÖNYVKIADÓ KFT.","Stephen King",2020,"Novella,elbeszélés", 1234, 264, 270);
-		Item item2 = new Book(4749 , "Álom doktor", "EURÓPA KÖNYVKIADÓ KFT.","Stephen King",2019,"Krimi, bûnügyi, thriller", 12334, 564, 810);
-		Item item3 = new Book(3199, "Érzéketlenül", "EURÓPA KÖNYVKIADÓ KFT.","Ed James",2020,"Krimi, bûnügyi, thriller", 123134, 420, 380);
-		Item item4 = new Book(2849, "Kedvencek temetõje", "EURÓPA KÖNYVKIADÓ KFT.","Stephen King",2019,"Krimi, bûnügyi, thriller", 123594, 587, 406);
-		Item item5 = new Book(2849, "Tortúra", "EURÓPA KÖNYVKIADÓ KFT.","Stephen King",2020,"Krimi, bûnügyi, thriller", 1234, 464, 322);
+		Item item1 = new Book(2849, "Emelkedï¿½s", "EURï¿½PA Kï¿½NYVKIADï¿½ KFT.","Stephen King",2020,"Novella,elbeszï¿½lï¿½s", 1234, 264, 270);
+		Item item2 = new Book(4749 , "ï¿½lom doktor", "EURï¿½PA Kï¿½NYVKIADï¿½ KFT.","Stephen King",2019,"Krimi, bï¿½nï¿½gyi, thriller", 12334, 564, 810);
+		Item item3 = new Book(3199, "ï¿½rzï¿½ketlenï¿½l", "EURï¿½PA Kï¿½NYVKIADï¿½ KFT.","Ed James",2020,"Krimi, bï¿½nï¿½gyi, thriller", 123134, 420, 380);
+		Item item4 = new Book(2849, "Kedvencek temetï¿½je", "EURï¿½PA Kï¿½NYVKIADï¿½ KFT.","Stephen King",2019,"Krimi, bï¿½nï¿½gyi, thriller", 123594, 587, 406);
+		Item item5 = new Book(2849, "Tortï¿½ra", "EURï¿½PA Kï¿½NYVKIADï¿½ KFT.","Stephen King",2020,"Krimi, bï¿½nï¿½gyi, thriller", 1234, 464, 322);
 		
 		webshop.getItemRepository().addItem(item1);
 		webshop.getItemRepository().addItem(item2);
@@ -19,16 +19,23 @@ public class WebshopApplication {
 		webshop.getItemRepository().addItem(item5);
 		WebshopService service = new WebshopService(webshop);
 		
-		service.RegisterUser("Ács Milán", "acsmilan@example.com");
-		service.RegisterUser("Álló Ella", "alloella@example.com");
+		service.RegisterUser("ï¿½cs Milï¿½n", "acsmilan@example.com");
+		service.RegisterUser("ï¿½llï¿½ Ella", "alloella@example.com");
 		service.RegisterUser("Vicc Elek", "viccelek@example.com");
 		
+		//user1
 		service.Login(webshop.getUserReposiotry().getUsers().get(0));
 		service.addItemToBasket(webshop.getItemRepository().getItems().get(0));
 		service.addItemToBasket(webshop.getItemRepository().getItems().get(2));
 		service.addItemToBasket(webshop.getItemRepository().getItems().get(4));
-		service.deleteItem(webshop.getItemRepository().getItems().get(2));
+		service.deleteItem(webshop.getItemRepository().getItems().get(2));	
+		service.PayBasket();
 		
+		//user2		
+		service.Login(webshop.getUserReposiotry().getUsers().get(1));
+		service.addItemToBasket(webshop.getItemRepository().getItems().get(1));
+		service.addItemToBasket(webshop.getItemRepository().getItems().get(2));
+		service.addItemToBasket(webshop.getItemRepository().getItems().get(3));		
 		service.PayBasket();
 				
 	}
